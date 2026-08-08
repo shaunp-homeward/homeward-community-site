2b3186fd033f54d5ef0abc99bbd32e29eb6a27ae
shaunp-homeward <shaunp@gmail.com>
2026-08-08 03:52:40 -0500

    Use approved V6 homepage renderer in V8 build

--- scripts/v8-build-hook.mjs
+++ scripts/v8-build-hook.mjs
@@ -1,12 +1,13 @@
 import { promises as fs } from 'node:fs';
-import { renderHomeV8, renderCirclesV8 } from './render-v8-front-door.mjs';
+import { renderHomeV6 } from './render-v8-home-v6.mjs';
+import { renderCirclesV8 } from './render-v8-front-door.mjs';
 
 const originalWriteFile = fs.writeFile.bind(fs);
 
 fs.writeFile = async (file, data, ...rest) => {
   const name = String(file);
   if (name.endsWith('/dist/index.html') || name.endsWith('\\dist\\index.html')) {
-    data = renderHomeV8(String(data));
+    data = renderHomeV6(String(data));
   } else if (name.endsWith('/dist/circles.html') || name.endsWith('\\dist\\circles.html')) {
     data = renderCirclesV8(String(data));
   }
