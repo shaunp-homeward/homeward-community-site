fdb9305687d6f747bd8267be89a625ec83118c1a
shaunp-homeward <shaunp@gmail.com>
2026-08-10 18:02:41 -0500

    Render V8 CMS inline formatting safely

--- scripts/v8-build-hook.mjs
+++ scripts/v8-build-hook.mjs
@@ -2,17 +2,18 @@ import { promises as fs } from 'node:fs';
 import { renderHomeV6 } from './render-v8-home-v6.mjs';
 import { renderCirclesPrimary } from './render-v8-circles-primary.mjs';
 import { renderPracticesPrimary } from './render-v8-practices-primary.mjs';
+import { applyCmsInlineFormatting } from './render-v8-inline-formatting.mjs';
 
 const originalWriteFile = fs.writeFile.bind(fs);
 
 fs.writeFile = async (file, data, ...rest) => {
   const name = String(file);
   if (name.endsWith('/dist/index.html') || name.endsWith('\\dist\\index.html')) {
-    data = renderHomeV6(String(data));
+    data = applyCmsInlineFormatting(renderHomeV6(String(data)));
   } else if (name.endsWith('/dist/circles.html') || name.endsWith('\\dist\\circles.html')) {
-    data = renderCirclesPrimary(String(data));
+    data = applyCmsInlineFormatting(renderCirclesPrimary(String(data)));
   } else if (name.endsWith('/dist/practices.html') || name.endsWith('\\dist\\practices.html')) {
-    data = renderPracticesPrimary(String(data));
+    data = applyCmsInlineFormatting(renderPracticesPrimary(String(data)));
   }
   return originalWriteFile(file, data, ...rest);
 };
