0b43416dbebf1adc61669666b6db96f175c084eb
shaunp-homeward <shaunp@gmail.com>
2026-08-10 14:44:32 -0500

    Wire V8 Practices page into primary build

--- scripts/v8-build-hook.mjs
+++ scripts/v8-build-hook.mjs
@@ -1,6 +1,7 @@
 import { promises as fs } from 'node:fs';
 import { renderHomeV6 } from './render-v8-home-v6.mjs';
 import { renderCirclesPrimary } from './render-v8-circles-primary.mjs';
+import { renderPracticesPrimary } from './render-v8-practices-primary.mjs';
 
 const originalWriteFile = fs.writeFile.bind(fs);
 
@@ -10,6 +11,8 @@ fs.writeFile = async (file, data, ...rest) => {
     data = renderHomeV6(String(data));
   } else if (name.endsWith('/dist/circles.html') || name.endsWith('\\dist\\circles.html')) {
     data = renderCirclesPrimary(String(data));
+  } else if (name.endsWith('/dist/practices.html') || name.endsWith('\\dist\\practices.html')) {
+    data = renderPracticesPrimary(String(data));
   }
   return originalWriteFile(file, data, ...rest);
 };
