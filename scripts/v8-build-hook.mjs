b85e95f76e164a9334c2f46cb69866c0e74187b9
shaunp-homeward <shaunp@gmail.com>
2026-08-10 19:11:31 -0500

    Route V8 Our Story through primary renderer

--- scripts/v8-build-hook.mjs
+++ scripts/v8-build-hook.mjs
@@ -2,6 +2,7 @@ import { promises as fs } from 'node:fs';
 import { renderHomeV6 } from './render-v8-home-v6.mjs';
 import { renderCirclesPrimary } from './render-v8-circles-primary.mjs';
 import { renderPracticesPrimary } from './render-v8-practices-primary.mjs';
+import { renderAboutPrimary } from './render-v8-about-primary.mjs';
 import { applyCmsInlineFormatting } from './render-v8-inline-formatting.mjs';
 
 const originalWriteFile = fs.writeFile.bind(fs);
@@ -14,6 +15,8 @@ fs.writeFile = async (file, data, ...rest) => {
     data = applyCmsInlineFormatting(renderCirclesPrimary(String(data)));
   } else if (name.endsWith('/dist/practices.html') || name.endsWith('\\dist\\practices.html')) {
     data = applyCmsInlineFormatting(renderPracticesPrimary(String(data)));
+  } else if (name.endsWith('/dist/about.html') || name.endsWith('\\dist\\about.html')) {
+    data = applyCmsInlineFormatting(renderAboutPrimary(String(data)));
   }
   return originalWriteFile(file, data, ...rest);
 };
